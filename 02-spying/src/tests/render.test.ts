d15ca12d6d093a64625e80987de14b9347681e08
Kristopher Gray <kristopher.gray99@gmail.com>
2023-03-17 10:59:49 +0100

    render todo test skeleton

---
+++ 02-spying/src/tests/render.test.ts
@@ -0,0 +1,23 @@
+/**
+ * render tests
+ */
+import { afterEach, describe, expect, it } from 'vitest'
+
+describe('render todos', () => {
+
+	it.todo('outputs empty list when no todos exist', () => {
+
+	})
+
+	it.todo('outputs a list with one todo', () => {
+
+	})
+
+	it.todo('outputs a list with one completed todo', () => {
+
+	})
+
+	it.todo('outputs a list with two todos', () => {
+
+	})
+})
