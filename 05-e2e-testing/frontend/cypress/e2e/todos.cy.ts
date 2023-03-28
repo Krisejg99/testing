741e4586c5b04440adfa7d03d6cd88725e054d27
Kristopher Gray <kristopher.gray99@gmail.com>
2023-03-28 14:54:17 +0200

    adjusted some expectations

--- 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
+++ 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
@@ -1,17 +1,22 @@
 describe('Todos', () => {
 	beforeEach(() => cy.visit('/'))
 
-	it('should see at least 1 todo', () => {
-		cy.get('#todos')
-			.find('li')
-			.should('have.length.at.least', 1)
+	context('initial state', () => {
+		it('should see at least 1 todo', () => {
+			cy.get('#todos')
+				.find('li')
+				.should('have.length.at.least', 1)
+		})
+		it('should not show the error dialog', () => {
+			cy.get('#error')
+				.should('not.be.visible')
+		})
 	})
 
 	context('create todo', () => {
 		it('create a todo form should be empty', () => {
-			cy.get('#new-todo-form')
-				.find('input')
-				.should('be.empty')
+			cy.get('#new-todo-title')
+				.should('have.value', '')
 		})
 
 		it('cant create a todo without a title', () => {
@@ -19,29 +24,33 @@ describe('Todos', () => {
 				.type('{enter}')
 
 			cy.get('#error')
-				.should('exist')
+				.should('be.visible')
 				.should('have.text', 'Title cannot be empty')
 		})
 
-		it('can create a new todo (and see it in the list)', () => {
-			cy.get('#new-todo-title')
-				.type('Eat, sleep, game, repeat...{enter}')
+		it('can create a new todo (and see it in the list and clears input)', () => {
+			const todoTitle = 'Eat, sleep, game, repeat...'
 
-			cy.get('#todos>li')
-				.eq(-1)
-				.should('contain.text', 'Eat, sleep, game, repeat...')
+			cy.get('#new-todo-title')
+				.type(`${todoTitle}{enter}`)
+				.should('have.value', '')
+
+			cy.get('#todos')
+				.find('li')
+				.last()
+				.should('contain.text', `${todoTitle}`)
 		})
 
 		it('can type in the create todo form and then reset the form', () => {
-			cy.get('#new-todo-form')
-				.find('input')
+			cy.get('#new-todo-title')
 				.type("Typing a todo, but as I'm typing it I realize I don't need this todo on the list...")
 
-			cy.get('#new-todo-form>button[type="reset"]')
+			cy.get('#new-todo-form')
+				.find('[type="reset"]')
 				.click()
 
 			cy.get('#new-todo-title')
-				.should('be.empty')
+				.should('have.value', '')
 		})
 	})
 })
